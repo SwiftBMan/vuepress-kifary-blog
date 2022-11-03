7de0ad584f3f9559ed3c6a8595e3d6360367a784
SwiftMan <zheng.qi.hui@qq.com>
2022-11-03 16:19:23 +0800

    初始化

--- config.ts
+++ config.ts
@@ -13,5 +13,5 @@ export default defineUserConfig({
                 str.replace(/^@root/, path.resolve(__dirname, '.')),
         },
     },
-    base: '/Blog-of-Kifary/'
+    base: '/vuepress-kifary-blog/'
 });
