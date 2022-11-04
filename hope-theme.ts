a7ef98d188d69ece731c6244fb83263bb1724a7a
SwiftMan <zheng.qi.hui@qq.com>
2022-11-04 18:11:14 +0800

    初始化

--- hope-theme.ts
+++ hope-theme.ts
@@ -88,9 +88,48 @@ export default hopeTheme({
         blog: {
             autoExcerpt: false,
         },
-        // mdEnhance: {
-        //     container: true
-        // }
+        mdEnhance: {
+            align: true,
+            attrs: true,
+            chart: true,
+            codetabs: true,
+            container: true,
+            demo: true,
+            echarts: true,
+            flowchart: true,
+            gfm: true,
+            imageLazyload: true,
+            imageTitle: true,
+            imageSize: true,
+            include: true,
+            katex: true,
+            mark: true,
+            mermaid: true,
+            playground: {
+                presets: ["ts", "vue"],
+            },
+            presentation: {
+                plugins: ["highlight", "math", "search", "notes", "zoom"],
+            },
+            stylize: [
+                {
+                    matcher: "Recommended",
+                    replacer: ({ tag }) => {
+                        if (tag === "em")
+                            return {
+                                tag: "Badge",
+                                attrs: { type: "tip" },
+                                content: "Recommended",
+                            };
+                    },
+                },
+            ],
+            sub: true,
+            sup: true,
+            tabs: true,
+            vPre: true,
+            vuePlayground: true,
+        },
         
     },
 
